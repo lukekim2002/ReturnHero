0ae8763c2ad08d6651e88392ce89bc4c35e178b0
Jikji <shken35@naver.com>
2018-09-18 17:51:39 +0900

    Create Astar Practice Folder

--- Assets/Scripts/GateKeeper/GateKeeper_Skill1_Manager.cs
+++ Assets/Scripts/GateKeeper/GateKeeper_Skill1_Manager.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GateKeeper_Skill_1_Manager : MonoBehaviour
+public class GateKeeper_Skill1_Manager : MonoBehaviour
 {
     #region PRIVATE
     private Vector2 _gateKeeperPos;
     private Vector2 _gateKeeperSkillDirection = new Vector2(0, -1);
     #endregion
 
-    private void GateKeeper_Skill_1_Excute1()
+    private void GateKeeper_Skill1_Ready()
     {
         _gateKeeperPos = this.transform.position;
         _gateKeeperPos.x += _gateKeeperSkillDirection.x * 0.32f;
@@ -17,7 +17,7 @@ public class GateKeeper_Skill_1_Manager : MonoBehaviour
         this.transform.position = _gateKeeperPos;
     }
 
-    private void GateKeeper_Skill_1_Excute2()
+    private void GateKeeper_Skill1_Excute()
     {
         _gateKeeperPos = this.transform.position;
         _gateKeeperPos.x += _gateKeeperSkillDirection.x * 4.18f;
