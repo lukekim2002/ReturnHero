1c2be877ad6bf65004c4e7ec48f243df0b9aad6d
lukekim2002 <lukekim2002@naver.com>
2018-10-02 15:37:51 +0900

    Map Manager

---
+++ Assets/Scripts/CheckEnteringRoom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckEnteringRoom : MonoBehaviour {
+
+   GameObject root;
+
+	// Use this for initialization
+	void Start ()
+    {
+        root = transform.root.gameObject;
+	}
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            root.SendMessage("SetChildrenIsTriggerOff", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
