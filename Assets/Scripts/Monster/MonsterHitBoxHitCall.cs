f494089b55c4b7a240af0cb9ca426f0bf872695b
lukekim2002 <lukekim2002@naver.com>
2018-12-27 23:23:37 +0900

    Editor Error Solved

---
+++ Assets/Scripts/Monster/MonsterHitBoxHitCall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterHitBoxHitCall : MonoBehaviour
+{
+    
+
+    public void GetHit(int damage)
+    {
+        transform.root.gameObject.SendMessage("HitByPlayer", damage, SendMessageOptions.DontRequireReceiver);
+    }
+}
