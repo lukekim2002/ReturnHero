f7cbaf6e01e3fe449e080f73a8d494f66024a9dc
lukekim2002 <lukekim2002@naver.com>
2019-01-28 21:04:34 +0900

    Cow Prefab

---
+++ Assets/Scripts/Monster/Stage2/Cow/CowSkillTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowSkillTrigger : MonoBehaviour
+{
+    GameObject root;
+    CowClass rootBehaviour;
+
+    private void Awake()
+    {
+        root = transform.root.gameObject;
+        rootBehaviour = root.GetComponent<CowClass>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            rootBehaviour._isSkill1TriggerOk = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            rootBehaviour._isSkill1TriggerOk = true;
+        }
+    }
+}
