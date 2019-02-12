799309999491098e90177471c69bfa4099d68b97
lukekim2002 <lukekim2002@naver.com>
2019-02-12 23:07:53 +0900

    Necomancer Skill1&2 Effect added

--- Assets/Scripts/Monster/Stage1/NecroMancer/NecromancerClass.cs
+++ Assets/Scripts/Monster/Stage1/NecroMancer/NecromancerClass.cs
@@ -19,7 +19,7 @@ public class NecromancerClass : MonsterBase {
     private float _Skill1CoolDown;
 
     private bool _isSkill2AttackReady;
-    private int _skill2Damage;
+    //private int _skill2Damage;
     private float _Skill2CoolDown;
 
     private Vector2 attackColliderSize;
@@ -55,15 +55,43 @@ public class NecromancerClass : MonsterBase {
     private void OnEnable()
     {
         myAction = Action.Idle;
-        myAttackCase = AttackCase.None;
 
 
+
+        Initialize();
+
+        aiMoveScript.enabled = false;
+
+        StartCoroutine(myBase.FindLookingDirection());
+
+        PlayerEnteredRoom();
     }
 
 
     // Update is called once per frame
     private void Update()
     {
+        myDirection = myBase.direction;
+
+        /*
+        if (_health <= 0)
+        {
+            DyingMotion();
+        }
+        */
+
+        if (myAction == Action.Idle && isAttacking == false)
+        {
+            myAnimator.SetInteger("actionNum", 0);
+            myAnimator.SetFloat("actionX", myDirection.x);
+            myAnimator.SetFloat("actionY", myDirection.y);
+        }
+        else if (myAction == Action.Move && isAttacking == false)
+        {
+            myAnimator.SetInteger("actionNum", 1);
+            myAnimator.SetFloat("moveX", myDirection.x);
+            myAnimator.SetFloat("moveY", myDirection.y);
+        }
 
     }
 
@@ -83,13 +111,82 @@ public class NecromancerClass : MonsterBase {
         myBase = GetComponent<MonsterBase>();
         if (myBase == null) Debug.LogError("myBase is null.");
 
+        myDataSet = MonsterDataManager.instance.ThrowDataIntoContainer((int)MonsterDataManager.MONSTER.NECROMANCER);
+        myColliderSet = CSVReader.Read("CSV/MonsterStage1/ReturnHero_Necromancer_AttackCollider");
 
+        _id = (int)myDataSet["ID"];
+        _health = (int)myDataSet["Health"];
+        _movingSpeed = (float)myDataSet["MovingSpeed"];
+        aiMoveScript.maxSpeed = _movingSpeed;
+
+        _meleeDamage = (int)myDataSet["MeleeDamage"];
+        _meleeCoolDown = (int)myDataSet["MeleeCoolDown"];
+
+        _skill1Damage = (int)myDataSet["Skill1Damage"];
+        _Skill1CoolDown = (int)myDataSet["Skill1CoolDown"];
+
+
+        _isMeleeAttackReady = true;
+        _isSkill1AttackReady = true;
+        //_isSkill1TriggerOk = false;
     }
 
 
     public override void AttackMelee()
     {
-        throw new System.NotImplementedException();
+        if (_isMeleeAttackReady == false && isAttacking == true) return;
+        _isMeleeAttackReady = false;
+
+        myAction = Action.Attack;
+        isAttacking = true;
+        //myAttackCase = AttackCase.Melee;
+        aiMoveScript.enabled = false;
+        myMeleeAttackRange.SetActive(false);
+
+        // Setting collider size and offset by its direction
+        switch (myLookingDirection)
+        {
+            case LookingDirection.Top:
+
+                attackColliderSize = new Vector2((float)myColliderSet[0]["Size_x"], (float)myColliderSet[0]["Size_y"]);
+                attackColliderOffset = new Vector2((float)myColliderSet[0]["Offset_x"], (float)myColliderSet[0]["Offset_y"]);
+
+                break;
+
+            case LookingDirection.Down:
+
+                attackColliderSize = new Vector2((float)myColliderSet[1]["Size_x"], (float)myColliderSet[1]["Size_y"]);
+                attackColliderOffset = new Vector2((float)myColliderSet[1]["Offset_x"], (float)myColliderSet[1]["Offset_y"]);
+
+                break;
+
+            case LookingDirection.Left:
+
+                attackColliderSize = new Vector2((float)myColliderSet[2]["Size_x"], (float)myColliderSet[2]["Size_y"]);
+                attackColliderOffset = new Vector2((float)myColliderSet[2]["Offset_x"], (float)myColliderSet[2]["Offset_y"]);
+
+                break;
+
+            case LookingDirection.Right:
+
+                attackColliderSize = new Vector2((float)myColliderSet[3]["Size_x"], (float)myColliderSet[3]["Size_y"]);
+                attackColliderOffset = new Vector2((float)myColliderSet[3]["Offset_x"], (float)myColliderSet[3]["Offset_y"]);
+
+                break;
+
+        }
+
+        myAnimator.SetInteger("actionNum", 2);
+        myAnimator.SetTrigger("isMelee");
+        myAnimator.SetFloat("actionX", myDirection.x);
+        myAnimator.SetFloat("actionY", myDirection.y);
+
+        attackCollider.SetActive(true);
+        attackColliderScript.size = attackColliderSize;
+        attackColliderScript.offset = attackColliderOffset;
+
+        StartCoroutine(WaitAnimationFinish());
+        StartCoroutine(CoolDownMelee());
     }
 
     public override void AttackSkill1()
@@ -105,27 +202,47 @@ public class NecromancerClass : MonsterBase {
     
     public override IEnumerator CoolDownMelee()
     {
-        throw new System.NotImplementedException();
+        yield return new WaitForSeconds(_meleeCoolDown);
+        _isMeleeAttackReady = true;
+        myMeleeAttackRange.SetActive(true);
     }
 
     public override IEnumerator CoolDownSkill1()
     {
-        throw new System.NotImplementedException();
+        yield return new WaitForSeconds(_Skill1CoolDown);
+        _isSkill1AttackReady = true;
     }
 
     public override IEnumerator CoolDownSkill2()
     {
-        throw new System.NotImplementedException();
+        yield return new WaitForSeconds(_Skill2CoolDown);
+        _isSkill2AttackReady = true;
     }
 
     public override void EndAttackMelee()
     {
-        throw new System.NotImplementedException();
+        myAction = Action.Move;
+        //myAttackCase = AttackCase.None;
+        myAnimator.SetInteger("actionNum", 1);
+        myAnimator.ResetTrigger("isMelee");
+        myAnimator.SetFloat("moveX", myDirection.x);
+        myAnimator.SetFloat("moveY", myDirection.y);
+        isAttacking = false;
+        aiMoveScript.enabled = true;
+
+        attackCollider.SetActive(false);
     }
 
     public override void EndAttackSkill1()
     {
-        throw new System.NotImplementedException();
+        myAction = Action.Move;
+        //myAttackCase = AttackCase.None;
+        myAnimator.SetInteger("actionNum", 1);
+        myAnimator.ResetTrigger("isSkill1");
+        myAnimator.SetFloat("moveX", myDirection.x);
+        myAnimator.SetFloat("moveY", myDirection.y);
+        isAttacking = false;
+        aiMoveScript.enabled = true;
     }
 
     public override void EndAttackSkill2()
@@ -133,29 +250,89 @@ public class NecromancerClass : MonsterBase {
         throw new System.NotImplementedException();
     }
 
-    public override void EndGetHit()
-    {
-        throw new System.NotImplementedException();
-    }
+    
 
     public override bool CheckAnimatorStateName(AnimatorStateInfo stateInfo)
     {
-        throw new System.NotImplementedException();
+        return (stateInfo.IsName("Melee")
+           || stateInfo.IsName("Skill")
+           || stateInfo.IsName("BeShot")
+           || stateInfo.IsName("Die"));
     }
 
     public override IEnumerator WaitAnimationFinish()
     {
-        throw new System.NotImplementedException();
+        AnimatorStateInfo stateInfo = myAnimator.GetCurrentAnimatorStateInfo(0);
+
+        // Wait Attack State
+        while (!CheckAnimatorStateName(stateInfo))
+        {
+            stateInfo = myAnimator.GetCurrentAnimatorStateInfo(0);
+            yield return null;
+        }
+
+        // Wait Animation Ends
+        while (stateInfo.normalizedTime <= 0.95f)
+        {
+            stateInfo = myAnimator.GetCurrentAnimatorStateInfo(0);
+            yield return null;
+        }
+
+        if (stateInfo.IsName("Melee"))
+            EndAttackMelee();
+        else if (stateInfo.IsName("Cow_Skill1_Down"))
+            EndAttackSkill1();
+        else if (stateInfo.IsName("BeShot"))
+            EndGetHit();
+        /*
+        else if(stateInfo.IsName("Die"))
+            gameObject.SetActive(false);
+            */
     }
 
     public override void DyingMotion()
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Die");
+
+
+        //myAnimator.SetInteger("actionNum", 3);
+        myAnimator.SetInteger("actionNum", 4);
+
+        StartCoroutine(WaitAnimationFinish());
+
+        gameObject.SetActive(false);
+    }
+
+    public override void EndGetHit()
+    {
+        //Debug.Log("GateKeeper EndGetHit");
+
+        myAction = Action.Move;
+        myAnimator.SetInteger("actionNum", 1);
+        myAnimator.SetFloat("moveX", myDirection.x);
+        myAnimator.SetFloat("moveY", myDirection.y);
+        aiMoveScript.enabled = true;
     }
 
     public override void HitByPlayer(int damage)
     {
-        throw new System.NotImplementedException();
+        myAction = Action.Idle;
+        aiMoveScript.enabled = false;
+
+        myAnimator.SetInteger("actionNum", 3);
+        myAnimator.SetFloat("actionX", myDirection.x);
+        myAnimator.SetFloat("actionY", myDirection.y);
+
+        _health -= damage;
+        Debug.Log("current health : " + _health);
+
+        StartCoroutine(WaitAnimationFinish());
+
+        if (_health <= 0)
+        {
+            // Dead
+            DyingMotion();
+        }
     }
 
     public override void GetHealed(GameGeneralManager.HealInfo myHeal)
@@ -211,7 +388,13 @@ public class NecromancerClass : MonsterBase {
         throw new System.NotImplementedException();
     }
 
-    
+
 
     #endregion
+
+    public void PlayerEnteredRoom()
+    {
+        myAction = Action.Move;
+        aiMoveScript.enabled = true;
+    }
 }
