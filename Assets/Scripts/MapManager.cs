550589083a169c01b0f4796101ae14463e7cd9c1
Jikji <shken35@naver.com>
2019-03-15 11:30:48 +0900

    Room Freeze

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -4,33 +4,67 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour {
 
-    public GameObject gateKeeper;
+    #region PRIVATE
+    private BoxCollider2D _wallBoxcollider2D;
+    private SpriteRenderer _wallSprite;
+    private bool _isRoomClear = false;
+    #endregion
 
-	// Use this for initialization
-	void Start ()
-    {
-        
-	}
+    #region PUBLIC
+    // 방 안에 있는 몬스터들
+    public GameObject[] monsters;
+    // 방 안에 있는 몬스터 개수
+    public int monsterNum;
+    #endregion
 
-    public void SetChildrenIsTriggerOn()
+    private void Start()
     {
-        for (int i = 0; i < transform.childCount; i++)
+        _wallBoxcollider2D = GetComponent<BoxCollider2D>();
+        _wallSprite = GetComponent<SpriteRenderer>();
+        monsterNum = monsters.Length;
+    }
+
+    //public void SetChildrenIsTriggerOn()
+    //{
+    //    for (int i = 0; i < transform.childCount; i++)
+    //    {
+    //        BoxCollider2D boxCollider2D = transform.GetChild(i).GetComponent<BoxCollider2D>();
+    //        boxCollider2D.isTrigger = true;
+    //    }
+    //}
+
+    //public void SetChildrenIsTriggerOff()
+    //{
+    //    for (int i = 0; i < transform.childCount; i++)
+    //    {
+    //        BoxCollider2D boxCollider2D = transform.GetChild(i).GetComponent<BoxCollider2D>();
+    //        boxCollider2D.isTrigger = false;
+
+    //        //gateKeeper.SetActive(true);
+    //    }
+    //}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_isRoomClear == false)
         {
-            BoxCollider2D boxCollider2D = transform.GetChild(i).GetComponent<BoxCollider2D>();
-            boxCollider2D.isTrigger = true;
+            if (collision.CompareTag("Player"))
+            {
+                _wallSprite.color = new Color(255, 255, 255, 255);
+                _wallBoxcollider2D.isTrigger = false;
+            }
         }
     }
 
-    public void SetChildrenIsTriggerOff()
+    public void CountMonsterInRoom()
     {
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            BoxCollider2D boxCollider2D = transform.GetChild(i).GetComponent<BoxCollider2D>();
-            boxCollider2D.isTrigger = false;
+        monsterNum--;
 
-            //gateKeeper.SetActive(true);
+        if (monsterNum == 0)
+        {
+            _wallSprite.color = new Color(255, 255, 255, 128);
+            _wallBoxcollider2D.isTrigger = true;
+            _isRoomClear = true;
         }
     }
-
-    
 }
