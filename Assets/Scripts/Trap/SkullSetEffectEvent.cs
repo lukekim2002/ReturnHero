88ac6f2e6677efb71fa37bf4dff01d823104acc9
lukekim2002 <lukekim2002@naver.com>
2019-04-02 21:20:33 +0900

    Stage1 Trap1 Script Attached

---
+++ Assets/Scripts/Trap/SkullSetEffectEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullSetEffectEvent : MonoBehaviour
+{
+    public GameObject Skeleton;
+
+    public void SpawnSkeltons()
+    {
+        Instantiate(Skeleton, transform.position, Quaternion.identity);
+    }
+
+    public void SetInactive()
+    {
+        if (transform.parent.gameObject.activeSelf == true)
+        {
+            transform.parent.gameObject.SetActive(false);
+        }
+    }
+}
