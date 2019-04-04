3a7d3589c3ac9f121ccb288cb724ef3008068c88
lukekim2002 <lukekim2002@naver.com>
2019-04-04 23:27:54 +0900

    Ice Spirit Prefab and functions
    1. 화살의 충돌 처리 미구현 -> 봉학이가 알려준다고 했음
    2. 애니메이터에서 트리거가 튀는 현상 발생함

---
+++ Assets/Scripts/Monster/Stage3/Ice/IceSpiritProjectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceSpiritProjectile : MonoBehaviour
+{
+    public int damage = 10;
+
+    private Vector2 _pos;
+    public Vector2 _dir;
+
+    public void Move1() // move 4px
+    {
+        _pos = this.transform.position;
+        _pos += _dir * 0.04f;
+        this.transform.position = _pos;
+    }
+
+    public void Move2() // move 50px
+    {
+        _pos = this.transform.position;
+        _pos += _dir * 0.5f;
+        this.transform.position = _pos;
+    }
+
+    public void Move3() // move 8x
+    {
+        _pos = this.transform.position;
+        _pos += _dir * 0.08f;
+        this.transform.position = _pos;
+    }
+
+    public void Delete()
+    {
+        Destroy(this.gameObject);
+    }
+}
